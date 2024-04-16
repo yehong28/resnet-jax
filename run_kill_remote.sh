35ccb691d7157f52b9cb40ff4e49945bf31e4dbc
KaimingHe <kmh.kaiminghe@gmail.com>
2024-04-16 06:05:36 +0000

    add kill remote

---
+++ run_kill_remote.sh
@@ -0,0 +1,13 @@
+echo 'To kill jobs in: '$VM_NAME 'in' $ZONE' after 2s...'
+sleep 2s
+
+echo 'Killing jobs...'
+gcloud compute tpus tpu-vm ssh $VM_NAME --zone $ZONE \
+    --worker=all --command "
+sudo pkill python
+sudo lsof -w /dev/accel0 | grep main.py | awk '{print \"sudo kill -9 \" \$2}' | sh
+sudo lsof -w /dev/accel0
+"
+echo 'Killed jobs.'
+
+# sudo lsof -w /dev/accel0 | grep main.py | awk '{print "sudo kill -9 " $2} | sh'
