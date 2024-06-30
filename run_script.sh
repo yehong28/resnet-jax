69da83ab77a01919da57c8d088e4b7bd6284eb6c
KaimingHe <kmh.kaiminghe@gmail.com>
2024-06-30 16:19:34 +0000

    add fake eu; set debug as False

--- run_script.sh
+++ run_script.sh
@@ -1,13 +1,16 @@
 # clean tmp
 rm -rf tmp
 
+# activate conda env
+conda activate resnet_jax
+
 PWD=$(pwd)
 python3 main.py \
-    --debug=True \
+    --debug=False \
     --workdir=${PWD}/tmp --config=configs/tpu.py \
     --config.dataset.cache=True \
     --config.dataset.root=./imagenet_fake \
-    --config.batch_size=4096 \
+    --config.batch_size=1024 \
     --config.dataset.prefetch_factor=2 \
     --config.dataset.num_workers=32 \
     --config.log_per_step=10 \
