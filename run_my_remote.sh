f593eb12acfd75ef8091c435d4402a9ef63415d5
yehong <yehong28@mit.edu>
2026-01-21 10:31:27 -0500

    run my remote script

---
+++ run_my_remote.sh
@@ -0,0 +1,39 @@
+# Run job in a remote TPU VM
+
+# v2, v3 VMs are deprecated
+# VM_NAME=kmh-tpuvm-v3-32-3
+# VM_NAME=kmh-tpuvm-v3-128-1
+# ZONE=europe-west4-a
+# VM_NAME=kmh-tpuvm-v2-32-1
+# ZONE=us-central1-a
+
+#VM_NAME=kmh-tpuvm-v4-32
+#ZONE=us-central2-b
+# SET ENV VARIABLES
+echo $VM_NAME $ZONE
+
+CONFIG=tpu
+STAGEDIR=/kmh-nfs-ssd-us-mount/code/yehong
+LOGDIR=${STAGEDIR}/tmp
+# some of the often modified hyperparametes:
+batch=4096
+lr=0.1
+ep=100
+PYTHON="/kmh-nfs-ssd-us-mount/code/yehong/miniconda3/envs/pde-tokenization/bin/python"
+
+gcloud compute tpus tpu-vm ssh $VM_NAME --zone $ZONE \
+    --worker=all --command "
+cd $STAGEDIR
+echo Current dir: $(pwd)
+export MPLCONFIGDIR=mlp_cache
+${PYTHON} resnet-jax/main.py \
+    --workdir=${LOGDIR} --config=resnet-jax/configs/${CONFIG}.py \
+    --config.dataset.root='/kmh-nfs-us-mount/data/imagenet' \
+    --config.batch_size=${batch} \
+    --config.num_epochs=${ep} \
+    --config.learning_rate=${lr} \
+    --config.dataset.prefetch_factor=2 \
+    --config.dataset.num_workers=32 \
+    --config.log_per_step=20 \
+    --config.model='ResNet50'
+" 
