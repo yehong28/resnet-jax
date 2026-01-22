e9891eecdbdc5ede4ee4256ed46bc4d0b4986599
yehong <yehong28@mit.edu>
2026-01-22 13:05:30 -0500

    imagefolder test

--- run_imagefolder_remote.sh
+++ run_imagefolder_remote.sh
@@ -1,4 +1,4 @@
-# Run job in a remote TPU VM
+# Run ImageFolder test in a remote TPU VM
 
 # v2, v3 VMs are deprecated
 # VM_NAME=kmh-tpuvm-v3-32-3
@@ -7,8 +7,8 @@
 # VM_NAME=kmh-tpuvm-v2-32-1
 # ZONE=us-central1-a
 
-#VM_NAME=kmh-tpuvm-v4-32
-#ZONE=us-central2-b
+# VM_NAME=kmh-tpuvm-v4-32
+# ZONE=us-central2-b
 # SET ENV VARIABLES
 echo $VM_NAME $ZONE
 
@@ -16,9 +16,7 @@ CONFIG=tpu
 STAGEDIR=/kmh-nfs-ssd-us-mount/code/yehong
 LOGDIR=${STAGEDIR}/tmp
 # some of the often modified hyperparametes:
-batch=4096
-lr=0.1
-ep=100
+batch=1024
 PYTHON="/kmh-nfs-ssd-us-mount/code/yehong/miniconda3/envs/pde-tokenization/bin/python"
 
 gcloud compute tpus tpu-vm ssh $VM_NAME --zone $ZONE \
@@ -26,18 +24,13 @@ gcloud compute tpus tpu-vm ssh $VM_NAME --zone $ZONE \
 cd $STAGEDIR/resnet-jax
 echo Current dir: $(pwd)
 export MPLCONFIGDIR=mlp_cache
-export TFDS_DATA_DIR='gs://kmh-gcp-us-central1/tensorflow_datasets'
-$PYTHON main.py \
+$PYTHON imagefolder_test.py \
         --debug=False \
-        --workdir=${PWD}/tmp --config=configs/tpu.py \
-        --config.dataset.cache=True \
-        --config.dataset.use_tfds=True \
-        --config.dataset.name=imagenet_fake_fake \
-        --config.dataset.root='gs://kmh-gcp-us-central1/tensorflow_datasets/' \
-        --config.batch_size=1024 \
+        --workdir=${LOGDIR} --config=configs/${CONFIG}.py \
+        --config.dataset.use_tfds=False \
+        --config.dataset.root='/kmh-nfs-us-mount/data/imagenet' \
+        --config.batch_size=${batch} \
         --config.dataset.prefetch_factor=2 \
-        --config.dataset.num_workers=32 \
-        --config.log_per_step=10 \
-        --config.model='_ResNet1'
+        --config.dataset.num_workers=32
 
 "
